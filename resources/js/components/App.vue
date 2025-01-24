b0b9539c02be9bec175890566ea7c95f98e447d7
Adarsh <100712252+adarshhaldar@users.noreply.github.com>
2025-01-25 05:01:10 +0530

    setup

---
+++ resources/js/components/App.vue
@@ -0,0 +1,5 @@
+<template>
+    <img src="https://media.tenor.com/3ydOlynD-2EAAAAM/dance-dance-boy.gif" alt="Party🥳">
+    <br>
+    <h1><b>🥳 Laravel + Vue Basic Integration Has Been Done, Congratulations! 🥳</b></h1>
+</template>
